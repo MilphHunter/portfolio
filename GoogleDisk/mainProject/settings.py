9bbaa8077ae872a443b81719f25a3774bdcdfc31
MilphHunter <dkomarov4444@gmail.com>
2024-08-22 13:16:59 +0300

    Removed the symbolic link to the portfolio site folder

--- GoogleDisk/mainProject/settings.py
+++ GoogleDisk/mainProject/settings.py
@@ -83,10 +83,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mynote',
-        'USER': 'note',
-        'PASSWORD': 'note'
+        'USER': 'postgres',
+        'PASSWORD': 'ALINAsunrise2004!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

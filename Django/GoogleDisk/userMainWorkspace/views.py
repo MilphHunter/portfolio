610132c1a7e4677d58250c2fc20135f2aedddfc5
MilphHunter <dkomarov4444@gmail.com>
2024-08-27 13:22:30 +0300

    add auto create folder

--- Django/GoogleDisk/userMainWorkspace/views.py
+++ Django/GoogleDisk/userMainWorkspace/views.py
@@ -270,3 +270,5 @@ def post_search(request):
                 search=search_query).order_by('-rank')
     return render(request, 'workspaces/workspace/find_notes.html',
                   {'search_form': search_form, 'tags': tags, 'query': query, 'notes': results})
+
+
